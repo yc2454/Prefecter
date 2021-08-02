4143ce6e2adb592d2c1580cebcf5f5b2f540679e
Yalu Cai <yc468@duke.edu>
2021-08-01 20:35:11 -0400

    store load bypass

--- graph.cc
+++ graph.cc
@@ -215,7 +215,8 @@ int main() {
     cout << "before pruning" << endl;
     print_graph(g, root);
     // store_load_bypassing(&g, root);
-    boost::remove_vertex(ld2, g);
+    boost::remove_vertex(const2, g);
+    boost::remove_vertex(const1, g);
     cout << "after pruning" << endl;
     print_graph(g, root);
 
