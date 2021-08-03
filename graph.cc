b41a32e13fb678b36c8c5bcd2cf294dea9d8b6f6
Yalu Cai <yc468@duke.edu>
2021-08-03 17:52:53 -0400

    check number of vertices

--- graph.cc
+++ graph.cc
@@ -141,6 +141,24 @@ void print_graph(Graph g, vertex_descriptor_t root) {
 
 }
 
+void remove_self_edge(Graph * g) {
+    vertex_descriptor_t vi, vi_end;
+    boost::tie(vi, vi_end) = boost::vertices(*g);
+
+    // self-edge. Don't understand why it exists yet
+    edge_descriptor_t start_self;
+    bool start_self_exists;
+
+    for (; vi != vi_end; vi++) {
+
+        boost::tie(start_self, start_self_exists) = boost::edge(vi, vi, *g);
+        if (start_self_exists) {
+            boost::remove_edge(start_self, *g);
+        }
+
+    }
+}
+
 void store_load_bypassing(Graph * g, vertex_descriptor_t root) {
 
     int num_sources;
@@ -230,10 +248,7 @@ void store_load_bypassing(Graph * g, vertex_descriptor_t root) {
                 cout << endl;
 
                 // reconnect the graph
-                boost::tie(start_self, start_self_exists) = boost::edge(start, start, *g);
-                if (start_self_exists) {
-                    boost::remove_edge(start_self, *g);
-                }
+                remove_self_edge(g);
                 // target_of_start = get_target(*g, start);
                 // add_edge(g, next, target_of_start);
                 // cout << "reconnect target " << target_of_start << " of start " << start << " to the next vertex " << next;
