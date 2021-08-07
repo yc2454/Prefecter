1e48cf4851db8c4368bb6dc408073d83a036d552
Yalu Cai <yc468@duke.edu>
2021-08-07 17:36:14 -0400

    changed definition of graph

--- graph.cc
+++ graph.cc
@@ -313,46 +313,6 @@ void store_load_bypassing(Graph * g, vertex_descriptor_t root) {
     
 }
 
-// print out the graph in matrix from
-void print_vertices(Graph *g) {
-    boost::graph_traits<Graph>::vertex_iterator vi, vi_end;
-    boost::tie(vi, vi_end) = boost::vertices(*g);
-
-    boost::graph_traits<Graph>::vertex_iterator ui, ui_end;
-
-    edge_descriptor_t e;
-    bool to, from;
-
-    boost::property_map<Graph, boost::vertex_bundle_t>::type pmap = boost::get(boost::vertex_bundle, *g);
-    VertexProperty vp;
-
-    cout << "  ";
-    for (; vi != vi_end; vi++) {
-        vp = boost::get(pmap, *vi);
-        cout << vp.source << " ";
-    }
-    cout << endl;
-
-    boost::tie(vi, vi_end) = boost::vertices(*g);
-    for (; vi != vi_end; vi++) {
-        vp = boost::get(pmap, *vi);
-        cout << vp.source << " ";
-        boost::tie(ui, ui_end) = boost::vertices(*g);
-        for (; ui != ui_end; ui++) {
-            boost::tie(e, to) = boost::edge(*ui, *vi, *g);
-            boost::tie(e, from) = boost::edge(*vi, *ui, *g);
-            if (to || from)
-                cout << 1 << " ";
-            else 
-                cout << 0 << " ";  
-        }
-        
-        cout << endl;
-    }
-
-    cout << endl;
-}
-
 void remove_vertex_in_func(Graph *g, vertex_descriptor_t v) {
     boost::remove_vertex(v, *g);
 }
