aa2cc14c6b501d157ac40fcb08270ee2fa450656
Yalu Cai <yc468@duke.edu>
2021-08-07 20:31:16 -0400

    changed definition of graph

--- graph.cc
+++ graph.cc
@@ -290,7 +290,7 @@ void store_load_bypassing(Graph * g, vertex_descriptor_t root) {
                 // cout << "the size of the circle is: " << circle.size() << endl;
 
                 for (int i = 1; i < circle.size(); i++) {
-                    cout << i  << " " << circle[i] << " ";
+                    // cout << i  << " " << circle[i] << " ";
                     boost::remove_vertex(circle[i], *g);
                 }
                 cout << endl;
