2fc78e6ce7ba8a299936cdfeeb9fd8f72811ab85
Yalu Cai <yc468@duke.edu>
2021-08-07 20:41:24 -0400

    changed definition of graph

--- graph.cc
+++ graph.cc
@@ -309,8 +309,8 @@ void store_load_bypassing(Graph * g, vertex_descriptor_t root) {
                 target = boost::target(*ei, *g);
                 cout << "got here 2" << endl;
 
-                // target_of_start = get_target(*g, start);
-                
+                target_of_start = target;
+
                 add_edge(g, next, target_of_start);
                 // cout << "reconnect target " << target_of_start << " of start " << start << " to the next vertex " << next;
                 
