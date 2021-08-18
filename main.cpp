fce0640c471ffa8cd50d7848eaaaa3c6d9977d6d
Yalu Cai <yc468@duke.edu>
2021-08-18 16:10:11 -0400

    start graph building

--- main.cpp
+++ main.cpp
@@ -520,6 +520,7 @@ int main(int argc, char** argv)
     Graph g = graph_create();
     cout << "building the graph" << endl;
     build_graph(last_occur_window, &g, miss_pc);
+    print_vertices(&g);
 
     cout << "finish\n";
     
