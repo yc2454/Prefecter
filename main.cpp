de5c0da3c17ea5b00c59413910f3f68da47c0dee
Yalu Cai <yc468@duke.edu>
2021-08-24 12:07:23 -0400

    crop out some cout's

--- main.cpp
+++ main.cpp
@@ -545,7 +545,11 @@ int main(int argc, char** argv)
     cout << "creating the graph" << endl;
     Graph g = graph_create();
     cout << "building the graph" << endl;
-    build_graph(last_occur_window, &g, miss_pc);
+    vertex_descriptor_t root = build_graph(last_occur_window, &g, miss_pc);
+    print_vertices(&g);
+
+    cout << "do compaction to the graph" << endl;
+    store_load_bypassing(&g, root);
     print_vertices(&g);
     
     return 0;
